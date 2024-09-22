e3e03b3b9e64e21a2777eaeb82640ced2dae0869
andrewh <mrrobottrax@gmail.com>
2024-09-22 14:02:32 -0400

    Only auto host when not in menu

--- Assets/Scripts/Networking/EditorAutoHost.cs
+++ Assets/Scripts/Networking/EditorAutoHost.cs
@@ -1,10 +1,13 @@
 ﻿// Start hosting when loading into a scene in the editor
+using UnityEngine.SceneManagement;
+
 internal static class EditorAutoHost
 {
 	public static void Init()
 	{
 #if UNITY_EDITOR
-		NetworkManager.StartHosting();
+		if (SceneManager.GetActiveScene().buildIndex != 0)
+			NetworkManager.StartHosting();
 #endif
 	}
 }
